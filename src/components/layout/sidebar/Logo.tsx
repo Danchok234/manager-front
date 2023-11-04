d7df14c87c482e3079749d4ccdc230bc15a6bade
Danchok234 <danaolejnik43@gmail.com>
2023-11-04 15:33:45 +0000

    sidebar changes

--- src/components/layout/sidebar/Logo.tsx
+++ src/components/layout/sidebar/Logo.tsx
@@ -4,8 +4,16 @@ import styles from './sidebar.module.scss'
 export default function Logo() {
 	return (
 		<div className={styles.logo}>
-			<Icon name='target' className='text-purple_basic min-w-[32px] min-h-[32px]' width={32} height={32} />
-			<span className='font-medium'>Manager</span>
+			<Icon
+				name='target'
+				className='text-purple_basic min-w-[32px] min-h-[32px]'
+				width={32}
+				height={32}
+			/>
+			<span className='overflow-visible whitespace-nowrap'>
+				<p className='font-medium'>Manager</p>
+				<p className='text-xs font-light text-grey_basic'>Created by D.Oliinyk</p>
+			</span>
 		</div>
 	)
 }
